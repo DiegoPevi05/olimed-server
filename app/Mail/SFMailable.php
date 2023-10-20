aef34f7f8959df7c52a8f8d955884e941dc09f36
Diego <diego10azul@hotmail.com>
2023-10-20 03:05:38 -0500

    deploy mail server

---
+++ app/Mail/SFMailable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SFMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function build()
+    {
+        return $this->view('emails.template', [
+                            'name' => $this->name,
+                           ])
+                    ->subject('Gracias por Contactarnos');
+    }
+}
