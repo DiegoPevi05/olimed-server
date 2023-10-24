13340e41b7fb8a81eba634e32a1498dd64d1c368
Diego <diego10azul@hotmail.com>
2023-10-23 23:20:14 -0500

    add support to language message

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SFMailable;
+use App\Mail\SFMailableEN;
 use App\Mail\SFMailableReply;
 
 class EmailController extends Controller
@@ -22,8 +23,9 @@ class EmailController extends Controller
         $email = $request->input('email');
         $content = $request->input('message');
         // Check for the "language" parameter
+        $language = $request->has('language') && $request->input('language') === 'es' ? 'es' : 'en';
 
-        $mailable = new SFMailable($name);
+        $mailable = $language === 'es' ? new SFMailable($name) : new SFMailableEN($name);
         Mail::to($email)->send($mailable);
 
         $mailableReply = new SFMailableReply($name, $email, $content);
